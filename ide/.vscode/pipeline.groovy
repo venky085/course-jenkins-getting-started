9ac91417b5170797ac4ac3d767724c0e3f86e6d8
Venkatesh Rajasekhar <venky085@gmail.com>
2022-10-14 08:56:46 -0400

    Add pipeline.groovy to SCM

---
+++ ide/.vscode/pipeline.groovy
@@ -0,0 +1,25 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git url: 'https://github.com/venky085/jgsu-spring-petclinic.git', branch: 'main'
+            }
+        }
+        stage('Build') {
+            steps {
+                bat './mvnw clean package'
+            }
+
+            post {
+                // If Maven was able to run the tests, even if some of the test
+                // failed, record the test results and archive the jar file.
+                always {
+                    junit '**/target/surefire-reports/TEST-*.xml'
+                    archiveArtifacts 'target/*.jar'
+                }
+            }
+        }
+    }
+}
