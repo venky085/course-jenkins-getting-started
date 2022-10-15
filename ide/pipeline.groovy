1a4d6645a659c69415312176d510bc539c4891bc
Venkatesh Rajasekhar <venky085@gmail.com>
2022-10-15 12:24:00 -0400

    initial email that is sent on every build

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -11,7 +11,8 @@ pipeline {
         }
         stage('Build') {
             steps {
-                bat './mvnw clean package'
+                sh './mvnw clean package'
+                //sh 'false'
             }
 
             post {
@@ -20,6 +21,14 @@ pipeline {
                 always {
                     junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
+                // }
+                // changed {
+                    // emailext subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}",
+                    //     body: 'Please go to ${BUILD_URL} and verify the build',
+                    //     attachLog: true,
+                    //     compressLog: true,
+                    //     to: 'test@jenkins',
+                    //     recipientProviders: [upstreamDevelopers(), requestor()]
                 }
             }
         }
