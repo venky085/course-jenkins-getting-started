ee4ff77f58112306cbe28f022e71e9ccff034df9
Venkatesh Rajasekhar <venky085@gmail.com>
2022-10-15 12:37:26 -0400

    refactor back to the actual build process with notifications on changed build status only

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -12,17 +12,15 @@ pipeline {
         stage('Build') {
             steps {
                 sh './mvnw clean package'
-                //sh 'false'
+                // sh 'false'
             }
 
             post {
-                // If Maven was able to run the tests, even if some of the test
-                // failed, record the test results and archive the jar file.
                 always {
                     junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
-                // }
-                // changed {
+                }
+                changed {
                     emailext subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}",
                         body: 'Please go to ${BUILD_URL} and verify the build',
                         attachLog: true,
