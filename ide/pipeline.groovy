bf7d1413b63d043568d6f15a16bded517e6e7188
Venkatesh Rajasekhar <venky085@gmail.com>
2022-10-15 12:25:28 -0400

    email

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -23,12 +23,12 @@ pipeline {
                     archiveArtifacts 'target/*.jar'
                 // }
                 // changed {
-                    // emailext subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}",
-                    //     body: 'Please go to ${BUILD_URL} and verify the build',
-                    //     attachLog: true,
-                    //     compressLog: true,
-                    //     to: 'test@jenkins',
-                    //     recipientProviders: [upstreamDevelopers(), requestor()]
+                    emailext subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}",
+                        body: 'Please go to ${BUILD_URL} and verify the build',
+                        attachLog: true,
+                        compressLog: true,
+                        to: 'test@jenkins',
+                        recipientProviders: [upstreamDevelopers(), requestor()]
                 }
             }
         }
